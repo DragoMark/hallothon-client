4f487f88efa63123782d104ee6003bb1e767d9c2
ThejasNU <thejasnu@gmail.com>
2021-10-23 22:38:33 +0530

    Home Component

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,47 @@
+import HomeSVG from "../Components/HomeSvg/HomeSVG";
+
 const Home = () => {
-	return <div className="text-5xl">Home</div>;
+	return (
+		<div className="home flex items-center justify-between px-6 w-auto h-screen bg-buttifyBG">
+			<div className="flex justify-between flex-col">
+				<div className="home__title font-noto text-3xl font-bold">
+					Tired of watching
+					<br /> Long videos to{" "}
+					<span className="text-buttifyGreen">study?</span>
+				</div>
+				<div className="home__description">
+					If you want to turn them to
+					<br /> a summarized text and QnA <br />
+					then
+				</div>
+				<div className="home__input">
+					{/* <input
+						type="text"
+						className="shadow-inner bg-transparent"
+						placeholder="Enter URL here"
+					/>
+					<button>summarize</button> */}
+					<form class="w-full max-w-sm">
+						<div class="flex items-center border-b border-teal-500 py-2">
+							<input
+								class="appearance-none bg-transparent border-none w-full text-gray-700 mr-3 py-1 px-2 leading-tight focus:outline-none"
+								type="text"
+								placeholder="Enter URL here"
+								aria-label="Full name"
+							/>
+							<button
+								class="flex-shrink-0 bg-buttifyGreen  hover:bg-buttifyGreen border-teal-500 hover:border-teal-700 text-sm border-4 text-black py-1 px-2 rounded"
+								type="button"
+							>
+								Summarize
+							</button>
+						</div>
+					</form>
+				</div>
+			</div>
+			<HomeSVG />
+		</div>
+	);
 };
 
 export default Home;
--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,12 @@
 import { Route, Router, Switch } from "react-router";
+import Navbar from "./Components/Navbar/Navbar";
 import Home from "./Pages/Home";
 import history from "./utils/history";
 
 const Routes = () => {
 	return (
 		<Router history={history}>
+			<Navbar></Navbar>
 			<Switch>
 				<Route path="/home" exact component={Home} />
 			</Switch>
